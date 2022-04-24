41e18d52ed568ed907b39e2d94eb58ca2e703667
Stephen Womack <swomack111@gmail.com>
2022-04-24 12:41:35 -0500

    defined basket and addItem listItems and empy functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,32 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+
+console.log(addItem('blueberries')) //checking if true
+console.log(basket) //checking if item added
+
+function listItems() {
+    for (let items of basket) {
+        console.log(`The items in your basket currently: ${basket}`);
+    }
+}
+
+listItems()  //checking if correct listing of items
+
+function empty(){
+    while (basket.length > 0){
+        basket.pop();
+        return 'emptied'
+    }
+}
+
+empty(); //emptied basket
+console.log(basket);  //checking if items are gone
+
